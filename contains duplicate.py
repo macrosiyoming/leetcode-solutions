13caeba48c504c9981af9b1eef468b8490b5a461
macrodyn4mic <marcodominicmagalona@gmail.com>
2026-05-29 16:16:57 +0800

    Delete contains duplicate.py

--- contains duplicate.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        myset = set()
-        for i in nums:
-            if i in myset:
-                return True
-            myset.add(i)
-        return False
-        
